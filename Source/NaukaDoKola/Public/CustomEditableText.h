fb6f539b9601ef8cb3d0fadb52d27e615207b1ce
MaciekBartoszNowakowski <macieknowakowski556@gmail.com>
2024-08-27 14:54:55 +0200

    proba dodania plikow po c++

---
+++ Source/NaukaDoKola/Public/CustomEditableText.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/EditableText.h"
+#include "CustomEditableText.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAUKADOKOLA_API UCustomEditableText : public UEditableText
+{
+	GENERATED_BODY()
+	
+};
