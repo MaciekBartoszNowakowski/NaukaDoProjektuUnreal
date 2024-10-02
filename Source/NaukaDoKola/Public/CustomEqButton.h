63226ca21d0b14ac3362683f5389e2c01bde5d2b
MaciekBartoszNowakowski <macieknowakowski556@gmail.com>
2024-10-02 11:48:19 +0200

    ekwipunek z okienkami opisu

---
+++ Source/NaukaDoKola/Public/CustomEqButton.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
+#include "Components/Overlay.h"
+#include "CustomEqButton.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAUKADOKOLA_API UCustomEqButton : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+    UPROPERTY(meta = (BindWidget))
+    UButton* Button;
+
+    UPROPERTY(meta = (BindWidget))
+    UOverlay* ChildContainer;
+
+ 
+    UFUNCTION(BlueprintCallable)
+    void AddChildToCustomButton(UWidget* Content)
+    {
+        if (ChildContainer && Content)
+        {
+            ChildContainer->AddChild(Content);
+        }
+    }
+	
+};
+
--- Source/NaukaDoKola/Public/CustomSortingFunction.h
+++ Source/NaukaDoKola/Public/CustomSortingFunction.h
@@ -8,16 +8,14 @@
 #include "CustomSortingFunction.generated.h"
 
 
-/**
- * 
- */
+
 UCLASS()
 class NAUKADOKOLA_API UCustomSortingFunction : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 
 public:
-	UFUNCTION(BlueprintCallable, Category="Custom|Sorting")
+	UFUNCTION(BlueprintCallable, Category = "Custom|Sorting")
 	static void SortEditableTextArrayByMyIntVariable(UPARAM(ref) TArray<UCustomEditableText*>& ArrayToSort);
-	
+
 };
